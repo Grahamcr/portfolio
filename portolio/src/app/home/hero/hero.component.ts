d2595600049c78ad6651ac2613d38f270a8a3744
Craig Graham <cgraham@vmox.com>
2019-12-18 10:46:30 -0500

    heading hero

--- portolio/src/app/home/hero/hero.component.ts
+++ portolio/src/app/home/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.less']
 })
 export class HeroComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() sub: string;
 
   constructor() { }
 
