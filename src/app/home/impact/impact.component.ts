cf4b870b435329a0da69e7a3688205240f80e36e
Craig Graham <cgraham@vmox.com>
2020-01-06 11:35:25 -0500

    about me

--- src/app/home/impact/impact.component.ts
+++ src/app/home/impact/impact.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ImpactComponent implements OnInit {
   @Input() stats: [string[][]];
+  @Input() title: string;
   constructor() { }
 
   ngOnInit() {
